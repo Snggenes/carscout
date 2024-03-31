75d8d608bb1c2e6057ff7e09771806bdc29bedd5
Enes <snggenes@gmail.com>
2024-03-31 03:02:47 +0200

    work on search--

--- src/components/search/Search.tsx
+++ src/components/search/Search.tsx
@@ -9,16 +9,14 @@ import { Button } from "../../../components/ui/button";
 import {
   Form,
   FormControl,
-  FormDescription,
   FormField,
   FormItem,
-  FormLabel,
   FormMessage,
 } from "../../../components/ui/form";
 import { useForm } from "react-hook-form";
 
 import { toast } from "react-toastify";
-import { Link, useNavigate } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 import { carData, prices, years } from "../../../lib/data";
 import { z } from "zod";
 import { zodResolver } from "@hookform/resolvers/zod";
